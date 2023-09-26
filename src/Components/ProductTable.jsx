543bbff6ffa23cc6373b11a883d4ddb1d026795e
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-09-26 10:39:53 +0300

    apiEvents

--- src/Components/ProductTable.jsx
+++ src/Components/ProductTable.jsx
@@ -1,88 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaEdit, FaTrash } from 'react-icons/fa';
-import AddProductButton from './AddProductButton'
-import '../Style/Product.scss'
+import AddProductButton from './AddProductButton';
+import '../Style/Product.scss';
 import UpdateProduct from './UpdateProduct';
+import axios from 'axios';
 
-class ProductTable extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isOpen: false,
-      products: [
-        {
-          id: 1,
-          name: 'Ürün 1',
-          category: 'Kategori 1',
-          description: 'Ürün 1 Açıklaması',
-          stock: 10,
-        },
-        {
-          id: 2,
-          name: 'Ürün 2',
-          category: 'Kategori 2',
-          description: 'Ürün 2 Açıklaması',
-          stock: 5,
-        },
-      ],
-    };
-  }
-  openComponent = () => {
-    this.setState({ isOpen: true });
+const ProductTable = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("https://localhost:7031/api/Products/")
+      .then((response) => {
+        console.log("API Response:", response.data);
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("API Error:", error);
+      });
+  }, []);
+  const openComponent = () => {
+    setIsOpen(true);
   };
 
-  closeComponent = () => {
-    this.setState({ isOpen: false });
+  const closeComponent = () => {
+    setIsOpen(false);
   };
-  handleUpdate(id) {
-    console.log('Güncelleme: ', id);
-  }
 
-  handleDelete(id) {
-    console.log('Silme: ', id);
-  }
-  render() {
-    return (
-      <div>
-        <h1>Ürün Listesi</h1>
-        <div className="product-table">
-          <table>
-            <thead>
-              <tr>
-                <th>Ürün Adı</th>
-                <th>Kategori</th>
-                <th>Açıklama</th>
-                <th>Stok</th>
-                <th></th>
+  const handleUpdate = (id) => {
+    // Update product with id
+  };
+
+  const handleDelete = (id) => {
+    // Delete product with id
+  };
+
+  return (
+    <div>
+      <h1>Ürün Listesi</h1>
+      <div className="product-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Ürün Adı</th>
+              <th>Kategori</th>
+              <th>Açıklama</th>
+              <th>Stok</th>
+              <th>Etiketler</th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {products.map((p) => (
+              <tr key={p.id}>
+                <td>{p.name}</td>
+                <td>{p.categoryName}</td>
+                <td>{p.description}</td>
+                <td>{p.stock}</td>
+                <td>{p.tagName.join(', ')}</td>
+                <td>
+                  <button style={{ backgroundColor: 'orange' }} className="update-button" onClick={() => {
+                    handleUpdate(p.Id);
+                    openComponent();
+                  }}>
+                    <FaEdit />
+                  </button>
+                  <button style={{ backgroundColor: 'red' }} className="delete-button" onClick={() => handleDelete(p.Id)}><FaTrash /></button>
+                </td>
               </tr>
-            </thead>
-
-            <tbody>
-              {this.state.products.map((product) => (
-                <tr key={product.id}>
-                  <td>{product.name}</td>
-                  <td>{product.category}</td>
-                  <td>{product.description}</td>
-                  <td>{product.stock}</td>
-                  <td>
-                    {/* Güncelleme düğmesi ve FaEdit ikonu */}
-                    <button style={{ backgroundColor: 'orange' }} className="update-button" onClick={() => {
-                      this.handleUpdate(product.id);
-                      this.openComponent();
-                    }}>
-                      <FaEdit />
-                    </button>
-                    <button style={{ backgroundColor: 'red' }} className="delete-button" onClick={() => this.handleDelete(product.id)}><FaTrash /></button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        {this.state.isOpen && <UpdateProduct onClose={this.closeComponent} />}
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  }
-}
-
+      {isOpen && <UpdateProduct onClose={closeComponent} />}
+    </div>
+  );
+};
 export default ProductTable;
