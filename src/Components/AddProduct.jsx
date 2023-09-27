5fb04e7bd7688444a2a0f66de74223ce48773500
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-09-27 15:05:18 +0300

    insertEvent

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -1,25 +1,49 @@
 import React, { useState, useRef } from 'react';
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdb-react-ui-kit';
-import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
-export default function ProductForm(props) {
+const AddProduct = (props) => {
     const [product, setProduct] = useState({
-        productName: '',
-        productDescription: '',
-        productCategory: '',
-        productStock: '',
+        name: '',
+        description: '',
+        categoryId: '', // Assuming categoryId is a number, change to: categoryId: 0,
+        tagNames: [], // Initialize tagNames as an empty array
+        stock: '' // Assuming stock is a number, change to: stock: 0,
     });
     const formRef = useRef();
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setProduct({ ...product, [name]: value });
+        if (name === 'tagNames') {
+            // If the input name is 'tagNames', split the input value by comma to create an array
+            const tagNamesArray = value.split(',').map(tag => tag.trim());
+            setProduct({ ...product, [name]: tagNamesArray });
+        } else {
+            setProduct({ ...product, [name]: value });
+        }
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Form verilerini işleme veya gönderme işlemini burada gerçekleştirin
-        console.log('Ürün Eklendi: ', product);
+
+        // Send a POST request to your API to create the product
+        axios
+            .post("https://localhost:7031/api/Products", product)
+            .then((response) => {
+                console.log('Ürün Eklendi: ', response.data);
+                // Optionally, you can reset the form fields after successful insertion
+                setProduct({
+                    name: '',
+                    description: '',
+                    categoryId: '', // Change to the initial value for categoryId
+                    tagNames: [], // Change to the initial value for tagNames
+                    stock: '', // Change to the initial value for stock
+                });
+            })
+            .catch((error) => {
+                console.error("API Error:", error);
+            });
+        console.log(product);
     };
 
     return (
@@ -32,9 +56,9 @@ export default function ProductForm(props) {
                             <MDBInput
                                 type="text"
                                 label="Ürün Adı"
-                                name="productName"
-                                value={product.productName}
+                                name="name"
                                 onChange={handleChange}
+                                value={product.name}
                             />
                         </MDBCol>
                     </MDBRow>
@@ -45,19 +69,23 @@ export default function ProductForm(props) {
                                     className="form-control"
                                     id="exampleFormControlTextarea1"
                                     rows="5"
-                                    name="productDescription"
-                                    value={product.productDescription}
+                                    name="description"
                                     onChange={handleChange}
                                     placeholder="Ürün Açıklaması"
+                                    value={product.description}
                                 />
                             </div>
                         </MDBCol>
                     </MDBRow>
                     <MDBRow className="mb-3">
                         <MDBCol>
-                            <Form.Select aria-label="Default select example">
-                                <option value={product.productCategory}>Kategori</option>
-                            </Form.Select>
+                            <MDBInput
+                                type="number"
+                                label="Kategori"
+                                name="categoryId"
+                                onChange={handleChange}
+                                value={product.categoryId}
+                            />
                         </MDBCol>
                     </MDBRow>
                     <MDBRow className="mb-3">
@@ -65,29 +93,21 @@ export default function ProductForm(props) {
                             <MDBInput
                                 type="number"
                                 label="Ürün Stok Sayısı"
-                                name="productStock"
-                                value={product.productStock}
+                                name="stock"
                                 onChange={handleChange}
+                                value={product.stock}
                             />
                         </MDBCol>
                     </MDBRow>
                     <MDBRow className="mb-3">
                         <MDBCol>
-                            <Form.Select aria-label="Default select example">
-                                <option value={product.productCategory}>Etiket</option>
-                            </Form.Select>
-                        </MDBCol>
-
-                        <MDBCol>
-                            <Form.Select aria-label="Default select example">
-                                <option value={product.productCategory}>Etiket</option>
-                            </Form.Select>
-                        </MDBCol>
-
-                        <MDBCol>
-                            <Form.Select aria-label="Default select example">
-                                <option value={product.productCategory}>Etiket</option>
-                            </Form.Select>
+                            <MDBInput
+                                type="text"
+                                label="Etiket"
+                                name="tagNames"
+                                onChange={handleChange}
+                                value={product.tagNames.join(', ')} // Display tagNames as a comma-separated string
+                            />
                         </MDBCol>
                     </MDBRow>
                     <MDBBtn color="success" type="submit">
@@ -95,7 +115,9 @@ export default function ProductForm(props) {
                     </MDBBtn>
                 </form>
             </MDBContainer>
-            <br></br>
+            <br />
         </div>
     );
 }
+
+export default AddProduct;
